ed526ba00e63d05b8e76ac0dc7820a24ca9a6171
Timo Tyrväinen <gr186703@gradia.fi>
2020-01-22 12:07:41 +0200

    Add project files.

---
+++ pyrämidi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyrämidi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.Write("pyramidi ");
+            int num, vali;
+            
+            {
+                
+                num = Convert.ToInt32(Console.ReadLine());
+                //vali = num ;
+                for (int i = 1; i <= num; i++)
+                {
+                    for (vali = 1; vali <= num ; vali++)
+                    {
+                        Console.Write("");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j);
+                    }
+                    {
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.ReadKey();
+            }
+
+            
+        }
+    }
+}
